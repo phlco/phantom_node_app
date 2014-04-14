9a52610ac3b24786c2409ef08665c55d957f641c
phlco <phillip.lamplugh@gmail.com>
2014-04-14 10:43:24 -0400

    updates tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,37 +4,26 @@ module.exports = function (grunt) {
       // Metadata.
       pkg: grunt.file.readJSON('package.json'),
 
-        // our Grunt task settings
-        // connect: {
-        //   test: {
-        //     port: 8000
-        //   }
-        // },
-        jasmine : {
-          src : 'lib/**/*.js',
-          options : {
-              specs : 'spec/**/*.js',
-              // template: require('grunt-template-jasmine-requirejs'),
-              // templateOptions: {
-              //     requireConfig: {
-              //         baseUrl: ''
-              //     }
-              // }
-          }
-        },
-        watch: {
-            js: {
-                files: [
-                    'src/**/*.js',
-                    'spec/**/*.js'
-                ],
-                tasks: ['jasmine']
-            }
+      jasmine : {
+        src : 'src/**/*.js',
+        options : {
+            specs : 'spec/**/*.js'
         }
+      },
+
+      watch: {
+        js: {
+          files: [
+            'src/**/*.js',
+            'spec/**/*.js'
+          ],
+          tasks: ['jasmine']
+        }
+      }
     });
+
      // Unit Testing Task
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    // grunt.loadNpmTasks('grunt-contrib-connect');
-    // grunt.registerTask('test', ['connect', 'jasmine']);
+
 };
